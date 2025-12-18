fa573f00631eb00bc15cbdbf02bfa09e14b3e7dc
Enric Petrus <enricpz@icloud.com>
2025-12-18 13:32:35 +0100

    clean up main file

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# third party libraries
+# Third party libraries
 import matplotlib.pyplot as plt       
 import numpy as np
 import yaml
 
-# local libraries
+# Local libraries
 from mk_module import (get_water_initial_concentrations, 
                        get_organophosphate_initial_concentrations,
                        create_reactions_for_step_one,
@@ -15,7 +15,7 @@ from plot_module import (plot_conc_vs_time,
                          plot_mk_information)
 
 def main():
-     ############### INPUT DEFINITION ################
+
      # Load YAML
      with open("config.yaml", "r") as f:
          config = yaml.safe_load(f)
@@ -27,31 +27,34 @@ def main():
      # Recreate NumPy array
      tr = config["timerange"]
      timerange = np.logspace(tr["start"], tr["stop"], num=tr["num"])
-     #################################################
      
+     # Define initial parameters
      delta_O_h2o = dkie["dh2o"]
      delta_O_cpo = dkie["dcpo"]
      ch2o, ccpo = dkie["conc_h2o"], dkie["conc_cpo"]
+
+     # Get initial concentrations
      initial_conc = get_water_initial_concentrations(delta_O_h2o, initial_C=ch2o)
-     initial_conc.update(get_organophosphate_initial_concentrations(delta_O_cpo, initial_C=ccpo))
+     tmp = get_organophosphate_initial_concentrations(delta_O_cpo, initial_C=ccpo)
+     initial_conc.update(tmp)
+
+     # Get reactions for the two steps
      reactions1 = create_reactions_for_step_one(dkie, verbose=False)
      reactions2 = create_reactions_for_step_two(dkie, verbose=False)
      total_reac = reactions1 + reactions2
      
-     for r in total_reac:
-         print(r)
- 
-     fig, axes = plt.subplots(
-         2, 2,
-         figsize=(14, 9),
-     )
+     # Set up and solve ODE equations
+     simdata = calculate_concentrations_from_mk(total_reac, initial_conc, timerange)
+     conc_data, t_data, compounds = simdata
 
-     concentration_data, time_data, compounds = calculate_concentrations_from_mk(total_reac, initial_conc, timerange)
-     plot_conc_vs_time(concentration_data, time_data, compounds, dkie, ax=axes[0,0])
-     plot_ratio_vs_reaction_progress(concentration_data, time_data, compounds, dkie, ax=axes[1,1])
-     plot_ratio_vs_time(concentration_data, time_data, compounds, dkie, ax=axes[1,0])
-     handles, labels = axes[0,0].get_legend_handles_labels()
-     plot_mk_information(dkie, handles, labels, ax=axes[0,1])
+     # Plot results
+     fig, axes = plt.subplots(2, 2, figsize=(14, 9))
+     ax1, ax2, ax3, ax4 = axes[0,0], [1,0], [0,1], [1,1]
+     plot_conc_vs_time(conc_data, t_data, compounds, dkie, ax=ax1)
+     plot_ratio_vs_reaction_progress(conc_data, t_data, compounds, dkie, ax=ax4)
+     plot_ratio_vs_time(conc_data, t_data, compounds, dkie, ax=ax2)
+     handles, labels = ax1.get_legend_handles_labels()
+     plot_mk_information(dkie, handles, labels, ax=ax3)
      plt.tight_layout()
      plt.savefig(outfile)
      plt.show()
