775b4af4a667fa388431f74f7908e2f9fe39733c
Enric Petrus <enricpz@icloud.com>
2026-08-12 14:52:00 +0200

    python project structure - reorganization

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import re
+
+with open("vizchemoton/__init__.py") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+setup(
+    name="toymodel",
+    version=version,
+    description="A toy model to run kinetic simulations regarding the oxygen isotope fractionation in phosphoryl transfer reactions",
+    author="Enric Petrus",
+    author_email=["enric.petrus@eawag.ch"],
+    # Tells setuptools to look inside the 'src' directory
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    
+    # Python version and dependencies
+    python_requires=">=3.8",
+    install_requires=[
+        "pyyaml>=6.0",
+        "matplotlib>=3.5",
+    ],
+    
+)
---
+++ src/toymodel/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This code is licensed under the 3-clause BSD license.
+Copyright Eawag, Department of Environmental Chemistry, 
+Hofstetter Group. See LICENSE.txt for details.
+"""
+
+__version__ = "1.0.0"
